5affd11f7aaa2606d238a2f036641850602ebdae
joyjoy <phatcharee2538@gmail.com>
2017-11-17 10:40:41 +0700

    Initial commit

---
+++ src/oop_lab/s359211110113/rmutsv/com/myFirstJAVA.java
@@ -0,0 +1,8 @@
+package oop_lab.s359211110113.rmutsv.com;
+
+public class myFirstJAVA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello wordl,I'm joy");
+    }//main
+} // class
